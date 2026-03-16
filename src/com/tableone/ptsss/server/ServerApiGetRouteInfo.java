350bd3635ec0479ba29360fd1cc64bfdb1f2a2d6
MelRyanGit <ryanmel0615@gmail.com>
2026-03-16 17:50:07 -0400

    Fixed variable declaration

--- src/com/tableone/ptsss/server/ServerApiGetRouteInfo.java
+++ src/com/tableone/ptsss/server/ServerApiGetRouteInfo.java
@@ -30,8 +30,8 @@ public class ServerApiGetRouteInfo extends ServerApi<String> {
         //reject any nonexsistant route numbers
         PreparedStatement ps = ServerDriver.query_routeExists();
         ps.setString(1, this.routeNumber);
-        ResultSet rs = ps.executeQuery();
-        if (!rs.next()) throw new Exception("Invalid Route Number!");
+        ResultSet Rnumber = ps.executeQuery();
+        if (!Rnumber.next()) throw new Exception("Invalid Route Number!");
 
         //fetch the query for this api call
         PreparedStatement info = ServerDriver.query_getRouteInfo();
