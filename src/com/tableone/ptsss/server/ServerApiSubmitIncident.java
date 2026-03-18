1a6d4b8730ec1e9f7b830223e7f96d048bddd7d6
dvzoob <59422689+dvzoob@users.noreply.github.com>
2026-03-17 22:57:46 -0700

    Add files via upload
    added support for multi-tagging

--- src/com/tableone/ptsss/server/ServerApiSubmitIncident.java
+++ src/com/tableone/ptsss/server/ServerApiSubmitIncident.java
@@ -1,19 +1,15 @@
 package com.tableone.ptsss.server;
 
+import java.security.Timestamp;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Time;
-import java.sql.Timestamp;
 import java.util.UUID;
 
-/*------------------------------------------------------------------------------*/
-/* ServerApiSubmitIncident                                                      */
-/* Author: David                                                                */
-/*                                                                              */
-/* submitIncident(stopLocation, time, description, tags[])                      */
-/* Submits an incident for a stop containing content tags, stop location,       */
-/* description, and time of incident. Returns the UUID of the created incident. */
-/*------------------------------------------------------------------------------*/
+/*
+ * This ServerAPI is used to submit an incident
+   submitIncident(string stopLocation);
+ */
 
 public class ServerApiSubmitIncident extends ServerApi<String> {
 
@@ -25,61 +21,65 @@ public class ServerApiSubmitIncident extends ServerApi<String> {
 	@Override
 	protected void parseRequest(Object[] args) throws Exception {
 
-		//check number of arguments
+		// check number of arguments
 		if (args.length != 4) {
 			throw new Exception("ServerApiSubmitIncidents received " + args.length + " arguments, expected 4");
 		}
 
-		// type-check stop location
+		// type check each argument
 		if (!(args[0] instanceof String)) {
-			throw new Exception("ServerApiSubmitIncidents received invalid type for stop location");
+			throw new Exception("ServerApiSubmitIncidents received invalid type for Stop Location");
 		}
-		
-		// type-check time
 		if (!(args[1] instanceof Timestamp)) {
-			throw new Exception("ServerApiSubmitIncidents received invalid type for time");
+			throw new Exception("ServerApiSubmitIncidents received invalid type for Time");
 		}
-		
-		// type-check description
 		if (!(args[2] instanceof String)) {
-			throw new Exception("ServerApiSubmitIncident received invalid type for description");
+			throw new Exception("ServerApiSubmitIncident received invalid type for Description");
 		}
-		
-		// type-check tags
 		if (!(args[3] instanceof String[])) {
-			throw new Exception("ServerApiSubmitIncident received invalid type for tags");
+			throw new Exception("ServerApiSubmitIncident received invalid type for Tags");
 		}
 
-		this.stopLocation = (String)args[0];
-		this.time = (Timestamp)args[1];
-		this.description = (String)args[2];
-		this.tags = (String[])args[3];
+		this.stopLocation = (String) args[0];
+		this.time = (Timestamp) args[1];
+		this.description = (String) args[2];
+		this.tags = (String[]) args[3];
 
 	}
-	
+
+	// this is the function that will finally fulfil the api call.
+	// you may return an object, not all apis will have to.
+	// if nothing has to be returned, simply return null instead.
+	// here, we will just return a string containing the name and age.
 	@Override
 	protected String completeRequest() throws Exception {
 		
-		//use transaction as this does CRUD operation
-		ServerDriver.beginTX();
+		try {
+			// use transaction as this is CRUD operation
+			ServerDriver.beginTX();
 
-		//fetch the query for this api call and set the parameters
-		PreparedStatement incidentPs = ServerDriver.query_submitIncident();
-		incidentPs.setString(1, this.stopLocation);
-		incidentPs.setTimestamp(2, this.time);
-		incidentPs.setString(3, this.description);
-		incidentPs.setString(4, this.tags);
-		String uuid = UUID.randomUUID().toString();
-		incidentPs.setString(5, uuid);
-		
-		//execute the query
-		incidentPs.execute();
-		
-		//commit transaction
-		ServerDriver.commitTX();
-		
-		return uuid;
-		
+			PreparedStatement incidentPs = ServerDriver.query_submitIncident();
+			incidentPs.setString(1, this.stopLocation);
+			incidentPs.setTimestamp(2, this.time);
+			incidentPs.setString(3, this.description);
+
+
+			String UUID = UUID.randomUUID().toString();
+			incidentPs.setString(5, UUID);
+			ResultSet rs = incidentPs.executeQuery();
+
+			// for each tag in tags[], add it 
+			for (String tag : this.tags) {
+				addIncidentTag.setString(1, UUID);
+				addIncidentTag.setString(2, tag);
+				addIncidentTag.execute();
+			}
+
+			ServerDriver.commitTX();
+
+			return UUID;
+		} catch (Exception e) {
+			return false;
+		}
 	}
-
 }
